1a316f14412cd6f780fdd6452f8848ae969f39c0
Yue Kang <axura.07@gmail.com>
2014-12-01 15:00:29 +1100

    used test.c to try getting binary inputs and then converting them directly into a uint8_t format. this will be used in the files for crctest.c

--- stuff/test.c
+++ stuff/test.c
@@ -2,12 +2,15 @@
 #include<ctype.h>
 #include<stdint.h>
 
+#define message_len 8
+
 uint8_t toBinary ( int* message );
 
 int main(void){
     char ch;
-    int message[8];
+    int message[message_len];
     int i = 0;
+    uint8_t sent;
     while (((ch = getchar()) != EOF) && (ch != '\n')){
 //        printf("%c\n",ch);
 //        ch = getchar();
@@ -15,17 +18,22 @@ int main(void){
         i++;
     }
 
-    for (i = 0; i < 8; i++){
+    for (i = 0; i < message_len; i++){
         printf("index %d: %d\n", i, message[i]);
     }
     
-//    printf ("message: %04x\n", message);
+    sent = toBinary(message);
+    printf ("message: %04x\n", sent);
 
     return 0;
 }
 
 uint8_t toBinary ( int* message ){
     uint8_t binary_message = 0;
-    
+    int i= 0;
+    for(i = 0; i < message_len; i++){
+        binary_message = (binary_message << 1) + message[0];
+    }
+    printf("hexadecimal value = %04x\n", binary_message);
     return binary_message;
 }
