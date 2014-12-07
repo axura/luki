9e201af02ad3cb8d490247b645b8d80e822f7c31
Yue Kang <axura.07@gmail.com>
2014-12-08 10:50:58 +1100

    updated main.c

--- crc/crc32bit/main.c
+++ crc/crc32bit/main.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
+#include<stdint.h>
 #include "crc32.c"
 
-int main (void){
-    unsigned char message = 0b11111110;
-    printf("message: %d\n",(int) message);
-    
-    unsigned int result;
-    result = crc32b( &message );
-    printf("result: %d\n", result);
-    
+#define input_len 28
+#define byte_len 8
+
+unsigned char toBinary ( unsigned char* message );
+
+int main (int argc, char *argv[]){
+    unsigned char message[byte_len];
+    int i;
+
+    for (i = 0; i < byte_len; i++){
+        printf("input from command line: %c\n", argv[1][i]);
+        message[i] = ((uint8_t) argv[1][i]) - '0';
+        printf("index %d: %d\n", i, message[i]);
+    }   
     return 0;
 }
 
+unsigned char toBinary ( unsigned char* message ){
+    unsigned char binary_message = 0;
+    int i= 0;
+    for(i = 0; i < byte_len; i++){
+        binary_message = (binary_message << 1) + message[i];
+//        printf("%d\n", binary_message);
+    }
+//    printf("hexadecimal value = %04x\n", binary_message);
+    return binary_message;
+}
