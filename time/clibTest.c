f408b13e2d7635cbcc7a302a424681b4f2495aab
Yue Kang <axura.07@gmail.com>
2015-01-07 16:50:28 +1100

    yay. The function has been tested for 4 bytes and 7 bytes long arrays. can convert them correctly, values are shown to be correct as well. Added  comments for the function

--- time/clibTest.c
+++ time/clibTest.c
@@ -27,32 +27,29 @@ int main(void)
         printf("%llu seconds since the epoch began \n", (unsigned long long) current_time);
     }
  
-    /* Convert to local time format. */
-    /*
-    c_time_string = ctime(&current_time);
- 
-    if (c_time_string == NULL)
-    {
-        (void) fprintf(stderr, "Failure to convert the current time.");
-        return EXIT_FAILURE;
-    }
-    */ 
-    /* Print to stdout. */
-    //(void) printf("Current time is %s", c_time_string);
-    
     //testing toByteArr function using random value
     unsigned long long test_int = 0x11DEADBEEF;
     printf("test_int = %llx\n", test_int);
     unsigned char test_array[unix_time_size]; //4 bytes long, this needs to be specified. 
     toByteArr( test_array, test_int, unix_time_size);
     
+    //testing crc, which is 3 bytes shorter
+    int crc_test = 0xDEAFBEAD;
+    unsigned char crc_array[4];
+    printf("\ncrc test value = %llx\n", (unsigned long long) crc_test);
+    toByteArr ( crc_array, (unsigned long long) crc_test , crc_size);
+     
+    
     return EXIT_SUCCESS;
 }
 
-//appending in the crc bits
+/* 
+    Function for converting any integer type data into a char array. 
+    This function is to be used for crc and unix time values mostly. 
+    See the constants for the sizes of each data variable type.    
+*/
 void toByteArr ( unsigned char* array, unsigned long long value , int array_size){
     int i;
-    //int j = 0;
     unsigned char ch;
     
     for( i = 0; i < array_size; i++){
@@ -60,16 +57,12 @@ void toByteArr ( unsigned char* array, unsigned long long value , int array_size
         array[i] = ch; 
     }
     
-    /*this was used previously in main.c for crc32 calculations
-    for(i = 1; i <= array_size; i++){
-        ch = value >> (value_bit_no - 8*i);
-        array[array_size + j] = ch;
-        j++;
-    }*/
-    
+    /*print code*/
     for(i=0; i < array_size; i++){
         printf("message[%d]: %04x\n", i, array[i]);
     }
+    /* end of print code */
+    
     return;
 }
 
